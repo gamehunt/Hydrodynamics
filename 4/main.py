d03db32bd89888305c7cc183833157f6fe4393dc
Lizord <mt.demakov@gmail.com>
2025-03-15 15:22:30 +0300

    finished 4

--- 4/main.py
+++ 4/main.py
@@ -1,5 +1,6 @@
+import math
+import tabulate
 import numpy as np
-import matplotlib
 import matplotlib.pyplot as plt
 
 def mu(i, j, grid):
@@ -13,7 +14,7 @@ def mu(i, j, grid):
         return y ** 2 + 4
 
     if j == grid.m // 4 and i <= grid.n // 2:
-        return x ** 2 + 1 / 16
+        return x ** 2 + 0.0625
 
     if j == grid.m:
         return x ** 2 + 1
@@ -30,7 +31,7 @@ def mu(i, j, grid):
     return 0
 
 def f(i, j, grid):
-    return 4
+    return -4
 
 def u(x, y):
     return x**2 + y**2
@@ -44,12 +45,14 @@ class Grid:
         self.h = w / n
         self.k = h / m
         self.k_sq = self.k ** 2
-        self.h_star = 1 / self.h ** 2
+        self.h_star = 1 / (self.h ** 2)
         self.k_star = 1 / self.k_sq
         self.a_star = -2 * (self.h_star + self.k_star)
         self.grid = []
         self.f = f
-        self.u  = u
+        self.u = u
+
+        self.accuracy = []
 
         self.x = np.linspace(0, self.width,  self.n + 1)
         self.y = np.linspace(0, self.height, self.m + 1)
@@ -79,9 +82,10 @@ class Grid:
         right = self.h_star * self.get(i + 1, j)
         up    = self.k_star * self.get(i, j + 1)
         down  = self.k_star * self.get(i, j - 1)
-        new   = (-self.f(i, j, self) - left - right - up - down) / self.a_star
+        f     = self.f(i, j, self)
+        new   = (f + left + right + up + down) / -self.a_star
         self.set(i, j, new)
-        return abs(prev - new)
+        return math.fabs(prev - new)
 
     def __opt_update(self, i, j):
         prev  = self.get(i, j)
@@ -92,15 +96,18 @@ class Grid:
         f     = self.f(i, j, self)
         new   = 0.4 * (f * self.k_sq + 0.25 * (left + right) + (up + down))
         self.set(i, j, new)
-        return abs(prev - new)
-
+        return math.fabs(prev - new)
 
     def solve(self, precision, max_iterations, optimized = False):
+        self.accuracy = []
+
         max_delta = precision + 1
         iterations = 0
+
         update_function = self.__update
         if optimized:
             update_function = self.__opt_update
+
         while iterations < max_iterations and max_delta > precision:
             max_delta = -1
             for j in range(1, self.m // 4 + 1):
@@ -114,46 +121,60 @@ class Grid:
                     if d > max_delta:
                         max_delta = d
             iterations += 1
+            self.accuracy.append(max_delta)
             print(f'i = {iterations}, z = {max_delta}')
 
-        d = None
+        error = None
         if self.u:
             z     = np.ravel(self.grid)
             diffs = np.absolute(np.subtract(z, self.uv))
-            d     = max(diffs[~np.isnan(diffs)])
-        return iterations, d
+            error     = max(diffs[~np.isnan(diffs)])
+
+        return iterations, max_delta, error
 
     def plot(self):
-        fig  = plt.figure()
+        fig  = plt.figure(1)
 
-        ax   = fig.add_subplot(1, 2, 1, projection='3d')
+        ax   = fig.add_subplot(1, 1, 1, projection='3d')
 
         z    = np.ravel(self.grid)
         Z    = z.reshape(self.X.shape)
 
-        ax.plot_surface(self.X, self.Y, Z, rstride = 1, cstride = 1)     
+        ax.plot_wireframe(self.X, self.Y, Z, color='b', rstride = 1, cstride = 1, label='~u(x, y)')     
         ax.scatter(self.X, self.Y, Z, color = 'red')
         ax.set_xlabel('X')
         ax.set_ylabel('Y')
         ax.set_zlabel('V')
-        ax.set_title('~u(x, y)')
+        # ax.set_title('~u(x, y)')
 
         if self.u:
             ZN = self.uv.reshape(self.X.shape)
-            ax = fig.add_subplot(1, 2, 2, projection='3d')
-            ax.scatter(self.X, self.Y, Z, color = 'red')
-            ax.plot_surface(self.X, self.Y, ZN, rstride = 1, cstride = 1, alpha = 0.5)     
-            ax.set_xlabel('X')
-            ax.set_ylabel('Y')
-            ax.set_zlabel('V')
-            ax.set_title('u(x, y)')
+            # ax = fig.add_subplot(1, 2, 2, projection='3d')
+            # ax.scatter(self.X, self.Y, Z, color = 'red')
+            # ax.scatter(self.X, self.Y, ZN, color = 'green')
+            ax.plot_wireframe(self.X, self.Y, ZN, color='g', rstride = 1, cstride = 1, alpha = 0.5, label='u(x, y)')     
+            # ax.set_xlabel('X')
+            # ax.set_ylabel('Y')
+            # ax.set_zlabel('V')
+            # ax.set_title('u(x, y)')
 
+        # fig = plt.figure(2)
+        # ax  = fig.add_subplot(1, 1, 1)
+        # ax.plot(np.arange(len(self.accuracy)), self.accuracy)
+        # ax.set_ylabel('Точность')
+        # ax.set_yscale('log')
+        # ax.set_xlabel('Итерация')
 
+        plt.legend()
         plt.show()
 
 if __name__ == '__main__':
     grid = Grid(2, 1, 8, 8, f, mu, u)
-    iterations, d = grid.solve(1e-14, 1000, True)
-    print(f'Solved in {iterations} iterations, delta = {d}')
+    max_iterations = 1000
+    iterations, acc, error = grid.solve(1e-14, max_iterations, True)
+    print(tabulate.tabulate([['Количество итераций', f'{iterations}/{max_iterations}'], 
+                             ['Точность', acc], 
+                             ['Погрешность', error]], 
+                            tablefmt='simple_grid', colalign=('left','right')))
     # print(grid)
     grid.plot()
