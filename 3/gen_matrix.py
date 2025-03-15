8142695b0e8fb2f473e845478e30c0b8c370c58f
Lizord <mt.demakov@gmail.com>
2025-03-15 15:50:32 +0300

    fix

--- 3/gen_matrix.py
+++ 3/gen_matrix.py
@@ -16,28 +16,38 @@ if __name__ == '__main__':
             row[41] = 'V' + indexes[i]
             row[42] = '-f' + indexes[i]
             row[i]  = 'A'
-            addend = 7
-            if i < 6:
-                addend = 3
+
+            # Левый сосед
             if i != 0 and i != 3 and i != 6 and i != 13 and i != 20 and i != 27 and i != 34:
                 row[i - 1] = '1/h^2'
             elif i == 0 or i == 3:
                 row[42] += '-mu5(y)/h^2'
             else:
                 row[42] += '-mu1(y)/h^2'
+
+            # Правый сосед
             if i != 2 and i != 5 and i != 12 and i != 19 and i != 26 and i != 33 and i != 40:
                 row[i + 1] = '1/h^2'
             else:
                 row[42] += '-mu2(y)/h^2'
-            if i > 2 and i < 6 or i > 9:
-                row[i - addend] = '1/k^2'
+
+            # Нижний сосед
+            if i > 2 and i < 6:
+                row[i - 3] = '1/k^2'
+            elif i > 9:
+                row[i - 7] = '1/k^2'
             elif i <= 2:
                 row[42] += '-mu6(x)/k^2'
             else:
                 row[42] += '-mu3(x)/k^2'
-            if i < 34:
-                row[i + addend] = '1/k^2'
+
+            # Верхний сосед
+            if i < 34 and i > 2:
+                row[i + 7] = '1/k^2'
+            elif i <= 2:
+                row[i + 3] = '1/k^2'
             else:
                 row[42] += '-mu4(x)/k^2'
+
             matrix.append(row)
         f.write(tabulate(matrix, headers="firstrow"))
