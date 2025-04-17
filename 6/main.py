752d60ae0b6d503c51b0d46342ef0254f4f9b438
Lizord <mt.demakov@gmail.com>
2025-04-17 17:59:58 +0300

    3

--- 6/main.py
+++ 6/main.py
@@ -52,6 +52,42 @@ def solve2(grid, a, h, tau):
             grid[t][x] = grid[t - 1][x] - a * tau * ((grid[t - 1][x + 1] - grid[t - 1][x - 1]) / (2 * h))
     return grid
 
+def solve_single(t, grid, a, h, tau):
+    prev = grid[t - 1]
+    curr = grid[t]
+
+    ti   = 1 / tau
+
+    mu1  = curr[0]
+    mu2  = 0
+
+    kappa1 = 0
+    kappa2 = 0
+
+    ai = [kappa1]
+    bi = [mu1]
+
+    C = -ti
+    A = -a / (4 * h)
+    B =  a / (4 * h)
+
+    for x in range(1, len(curr) - 1):
+        phi  = prev[x] * ti - a / (4 * h) * (prev[x + 1] - prev[x - 1])
+        alpha = B / (C - A * ai[x - 1])
+        beta  = (-phi + A * bi[x - 1]) / (C - A * ai[x - 1])
+        ai.append(alpha)
+        bi.append(beta)
+
+    curr[-1] = (kappa2 * bi[-1] + mu2) / (1 - kappa2 * ai[-1])
+    for x in range(len(curr) - 2, -1, -1):
+        curr[x] = ai[x] * curr[x + 1] + bi[x]
+
+
+def solve3(grid, a, h, tau):
+    for t in range(1, len(grid)):
+        solve_single(t, grid, a, h, tau)
+    return grid
+
 def generate_precise(u0, a, i, h, n, tau):
     grid = []
     for x in range(n):
@@ -67,10 +103,11 @@ m = h * 20
 # tau / h < 0.5
 
 if __name__ == '__main__':
-    target = u0_0
+    target = u0_2
 
     grid1, hx, tau = create_grid(w, h, n, m, target, mu)
     grid2, hx, tau = create_grid(w, h, n, m, target, mu)
+    grid3, hx, tau = create_grid(w, h, n, m, target, mu)
 
     c = abs(a) * tau / hx
     if c > 1:
@@ -80,6 +117,7 @@ if __name__ == '__main__':
 
     grid1 = solve1(grid1, a, hx, tau)
     grid2 = solve2(grid2, a, hx, tau)
+    grid3 = solve3(grid3, a, hx, tau)
 
     fig, ax = plt.subplots()
     x = np.linspace(0, w, n)
@@ -89,6 +127,7 @@ if __name__ == '__main__':
         plt.cla()
         ax.plot(x, grid1[frame], '-', label = 'method 1')
         # ax.plot(x, grid2[frame], '-', label = 'method 2')
+        ax.plot(x, grid3[frame], '-', label = 'method 3')
         ax.plot(x, generate_precise(target, a, frame, hx, n, tau), '-', label = 'precise')
         plt.legend()
 
